c932deda3fa5d11b406ddf1a32635c15d0b034e5
Numan381 <mnuman.202207023@gcuf.edu.pk>
2024-03-03 14:22:27 +0500

    NUman

--- src/components/Coins.jsx
+++ src/components/Coins.jsx
@@ -1,11 +1,64 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { server } from "../index";
+import {
+  Container,
+  HStack,
+} from "@chakra-ui/react";
+import Loader from "./Loader";
+import ErrorComponent from "./ErrorComponent";
+import CoinCard from "./CoinCard"
 
 const Coins = () => {
+  const [coins, setCoins] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+  const [page, setPage] = useState(1);
+  const [currency, setCurrency] = useState("inr");
+
+  const currencySymbol = currency==="inr"?"₹":currency==="eur"?"€":"$";
+
+  useEffect(() => {
+    const fetchCoins = async () => {
+      try {
+        const { data } = await axios.get(`${server}/coins/markets?vs_currency=${currency}&page=${page}`);
+        setCoins(data);
+        setLoading(false);
+      } catch (error) {
+        setError(true); 
+        setLoading(false);
+      }
+    };
+    fetchCoins();
+  }, [currency, page]);
+
+  if (error) return <ErrorComponent/>
+
   return (
-    <div>
-      coins
-    </div>
-  )
-}
+    <Container maxW={"container.xl"}>
+      {loading ? (
+        <Loader />
+      ) : (
+        <>
+          <HStack wrap={"wrap"}>
+            {coins.map((i) => (
+              <CoinCard
+              id={i.id}
+                key={i.id}
+                name={i.name}
+                img={i.image}
+               price={i.current_price}
+               symbol={i.symbol}
+              currencySymbol={currencySymbol}
+              />
+            ))}
+          </HStack>
+        </>
+      )}
+    </Container>
+  );
+};
+
+
 
 export default Coins
---
+++ src/components/ErrorComponent.jsx
@@ -0,0 +1,8 @@
+import { Heading } from "@chakra-ui/react";
+import React from "react";
+
+const ErrorComponent = () => {
+  return <Heading bg={'red'}>404 page not found!</Heading>;
+};
+
+export default ErrorComponent;
