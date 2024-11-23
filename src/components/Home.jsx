194c145225935e3afd8bb30c238156a8281c86b1
Numan381 <mnuman.202207023@gmail.com>
2024-11-23 04:24:04 -0800

    yep

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,10 +2,9 @@ import { Box, Image, Text } from '@chakra-ui/react'
 import React from 'react'
 import btcSrc from "../assets/btc.png"
 const Home = () => {
-  return <Box bg={"blackAlpha.900"} w={"full"} h={"85vh"}>
+  return <Box bg={"blackAlpha.900"} w={"full"} h={"88.7vh"} overflow={'hidden'}>
 <Image w={"full"} h={"full "} filter={"grayscale(1)"} objectFit={"contain"} src={btcSrc}/>
-<Text fontSize={"6xl"} textAlign={"center"} fontWeight={"thin"} color={"whiteAlpha.700"} mt={"-20"}>nomi</Text>
-
+<Text fontSize={"6xl"} textAlign={"center"} fontWeight={"thin"} color={"whiteAlpha.700"} mt={"-20"}>Coin</Text>
   </Box>
 }
  export default Home
